451b822421585d16e85be24af9015f4d33bcf928
Connor George <connor.v.george1@gmail.com>
2025-03-17 21:47:24 -0700

    Added code to talk to the flask backend and display a message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
 
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://34.219.168.65:5000")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("Failed to load message");
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,7 @@ function App() {
         <p>
           Well hello there, Sammy.  Perhaps we will find some boats soon...
         </p>
+        <p>{message}</p>
       </header>
     </div>
   );
